da5ce41d29e07234681d2b39ed50883b5c762777
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2024-04-25 10:36:08 -0700

    Update Spreadsheet component for ag-grid

--- components/Spreadsheet.tsx
+++ components/Spreadsheet.tsx
@@ -1,24 +1,93 @@
 "use client";
 import { useEffect, useState } from "react";
-import * as SprdPackage from "react-spreadsheet";
 import { invoke } from "@tauri-apps/api/tauri";
-
-const DefaultSpreadsheet = SprdPackage.Spreadsheet;
+import { AgGridReact } from "ag-grid-react"; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import {
+  CellEditingStoppedEvent,
+  ColDef,
+  ColGroupDef,
+} from "ag-grid-community";
 
 const Spreadsheet = () => {
-  const [data, setData] = useState([
-    [{ value: "Vanilla" }, { value: "Chocolate" }, { value: "" }],
-    [{ value: "Strawberry" }, { value: "Cookies" }, { value: "" }],
-  ]);
-  // const onSubmitValues = () => {
-  //   invoke<number>("pmt_calculation", { rate: 0.05, nper: 5, pv: 1000, fv: 0 })
-  //     .then((result) => console.log(result))
-  //     .catch(console.error);
-  // };
-  const onChange = (data: SprdPackage.Matrix<{ value: string }>) => {
-    console.log(data);
+  const getPmt = async ({
+    rate,
+    nper,
+    pv,
+    fv,
+  }: {
+    rate: number;
+    nper: number;
+    pv: number;
+    fv: number;
+  }) => {
+    return new Promise<number>((resolve, reject) => {
+      invoke<number>("pmt_calculation", { rate, nper, pv, fv })
+        .then((result) => resolve(result))
+        .catch(reject);
+    });
   };
-  return <DefaultSpreadsheet data={data} onChange={onChange} />;
+  const [rowData, setRowData] = useState([
+    { label: "Loan", value: "1000000" },
+    { label: "Interest Rate (Annual)", value: "6.0" },
+    { label: "Term (Yrs)", value: "30" },
+    { label: "Monthly Payment", value: "-5995.51" },
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState<
+    (
+      | ColDef<{ label: string; value: string }, any>
+      | ColGroupDef<{ label: string; value: string }>
+    )[]
+  >([
+    { headerName: "Label", field: "label" },
+    {
+      headerName: "Value",
+      field: "value",
+      editable: true,
+    },
+  ]);
+
+  const onCellEdit = (
+    event: CellEditingStoppedEvent<
+      | { label: string; value: string; editable?: undefined }
+      | { label: string; value: string; editable: boolean },
+      any
+    >
+  ) => {
+    const { value } = event;
+    const loan = rowData.find((row) => row.label === "Loan")?.value;
+    const interestRate = rowData.find(
+      (row) => row.label === "Interest Rate (Annual)"
+    )?.value;
+    const term = rowData.find((row) => row.label === "Term (Yrs)")?.value;
+    // Get PMT based on the new value
+    if (loan && interestRate && term) {
+      getPmt({
+        rate: parseFloat(interestRate) / 100 / 12,
+        nper: parseFloat(term) * 12,
+        pv: -parseFloat(loan),
+        fv: 0,
+      }).then((result) => {
+        rowData[3].value = (-result).toString();
+        setRowData(() => [...rowData]);
+      });
+    }
+  };
+  return (
+    <div
+      className="ag-theme-quartz" // applying the grid theme
+      style={{ height: 300, width: 450 }} // the grid will fill the size of the parent container
+    >
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        onCellEditingStopped={onCellEdit}
+      />
+    </div>
+  );
 };
 
 export default Spreadsheet;
